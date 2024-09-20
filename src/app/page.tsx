effba2950e4f38d66ce1563a136d56588f29608f
hayato94087 <hayato94087@gmail.com>
2024-09-20 21:36:41 +0900

    ページを変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import { type FC } from "react";
 
+import { env } from "@/env";
+
 const Home: FC = () => {
+  const url = env.DEBUG_URL;
+
   return (
     <main>
       <div className="flex h-screen items-center justify-center">
         <div className="flex flex-col items-center">
           <h1 className="text-2xl font-bold">Hello Universe</h1>
+          <p className="text-sm text-gray-500">{url}</p>
         </div>
       </div>
     </main>
